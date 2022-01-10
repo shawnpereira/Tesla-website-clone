bb041d3b6bd053826ae6a24567e8f8d51a439f59
Shawn Pereira <shawnpereiraswp@gmail.com>
2022-01-11 00:22:22 +0530

    Initial commit

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
 
 function Header() {
   return (
-    <div>
-      Header
-    </div>
-  )
+    <Box>
+      <a>
+        <img src="/images/logo.svg" alt=""></img>
+      </a>
+      <Menu>
+        
+      </Menu>
+    </Box>
+  );
 }
 
-export default Header
+export default Header;
+const Box = styled.div`
+  min-height: 60px;
+  position: fixed;
+  display: flex;
+  align-items: center;
+  padding: 0 20px;
+`;
+
+const Menu = styled.div``;
